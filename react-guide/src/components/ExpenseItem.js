57cc8164a8e4293504d4633942f06621583e10f4
newExpand <magin0auto@gmail.com>
2023-01-02 22:59:46 +0900

    feat: 날짜 UI 컴포넌트 분리 및 props 값 전달

--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const year = props.date.getFullYear(),
-    month = props.date.toLocaleString('ko-KR', {month: 'short'}),
-    day = props.date.toLocaleString('ko-KR', {day: '2-digit'});
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}원</div>
