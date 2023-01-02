708990b6130c05a0eb4434caf07acdfa67bbcb17
newExpand <magin0auto@gmail.com>
2023-01-02 22:28:17 +0900

    feat: 하나로 쓰여진 일자 년,월,일 분리

--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+    const year = props.date.getFullYear(),
+    month = props.date.toLocaleString('ko-KR', {month: 'short'}),
+    day = props.date.toLocaleString('ko-KR', {day: '2-digit'});
+
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}원</div>
