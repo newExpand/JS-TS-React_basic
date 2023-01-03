1a8d1cf8578cacda330ea2293f8e032ec40a3520
newExpand <magin0auto@gmail.com>
2023-01-03 14:43:20 +0900

    feat: useState 사용하여 값 변경

--- react-guide/src/components/Expenses/ExpenseItem.js
+++ react-guide/src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,26 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseAmount from "./ExpenseAmount";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked");
+    setTitle('업데이트!')
   };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <ExpenseAmount amount={props.amount} />
       </div>
-      <button type='button' onClick={clickHandler}>제목 변경</button>
+      <button type='button' onClick={clickHandler}>
+        제목 변경
+      </button>
     </Card>
   );
 };
