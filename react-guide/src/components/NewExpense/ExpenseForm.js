afe81456408b34b72bf4f979858e9205d52be751
newExpand <magin0auto@gmail.com>
2023-01-22 23:09:45 +0900

    refactor: 폼 제출 처리 객체로 일괄처리

--- react-guide/src/components/NewExpense/ExpenseForm.js
+++ react-guide/src/components/NewExpense/ExpenseForm.js
@@ -2,47 +2,54 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
 
   const titleChangeHandler = (e) => {
-    // setEnteredTitle(e.target.value);
+    setEnteredTitle(e.target.value);
     // setUserInput({
     //     ...userInput,
     //     enteredTitle: e.target.value
     // })
-
-    // 변화되는 값을 가장 최신상태의 값으로 유지하는 방법
-    setUserInput((prevState) => {
-        return {...prevState, enteredTitle: e.target.value}
-    })
   };
 
   const amountChangeHandler = (e) => {
-    // setEnteredAmount(e.target.value);
-    setUserInput({
-        ...userInput,
-        enteredAmount: e.target.value
-    })
+    setEnteredAmount(e.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredAmount: e.target.value
+    // })
   };
 
   const dateChangeHandler = (e) => {
-    // setEnteredDate(e.target.value);
-    setUserInput({
-        ...userInput,
-        enteredDate: e.target.value
-    })
+    setEnteredDate(e.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredDate: e.target.value
+    // })
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData)
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>제목</label>
